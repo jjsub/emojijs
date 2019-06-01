ac26857ea7a5108908c736d0178006dc26d895b9
dw-jjir <dw-jjir@dw-jjirs-MBP.attlocal.net>
2019-06-01 18:45:25 -0500

    Review the used of array distructure
    Long time no see

--- emoji.js
+++ emoji.js
@@ -1,14 +1,18 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
 const emoji = require('node-emoji');
 
 const { log } = console;
-
+log("This is the Logf", process.argv);
 if (process.argv.length <= 2) {
   log(`Usage: ${__filename} emoji_text`);
   process.exit(-1);
 }
 
-const result = emoji.emojify(process.argv[2]);
+// const result = emoji.emojify(process.argv[2]);
+
+const [, , ...argArray] = process.argv;
+const message = argArray.join(' ');
+const result = emoji.emojify(message);
 
 log(result);
