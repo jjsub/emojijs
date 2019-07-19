fb7089ce77501ec59d1843c5079831b8fbf39ea7
dw-jjir <dw-jjir@dw-jjirs-MacBook-Pro.local>
2019-07-19 18:14:34 -0500

    Word with readline and feed CLI with external file as argument

---
+++ emjoiSTDIN.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+const emoji = require('node-emoji');
+
+const {log} = console;
+
+if (process.stdin.isTTY || process.env.STDIM === '0') {
+    if (process.argv.length <= 2) {
+        log(`Usage: ${__filename} emoji_text`);
+        process.exit(-1);
+    }
+    const [,, args] = process.argv;
+    const message = args.join(' ');
+    const result = emoji.emojify(message);
+    log(result);
+} else {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
+
+    rl.on('line', line => {
+        const result = emoji.emojify(line);
+        log(result);
+    });
+}
+
+// log("process.stdin", process.stdin);
+// log("process.stdout", process.stdout);
+
+// Note: run $ cat message.txt | node emjoiSTDIN.js 
--- emoji.js
+++ emoji.js
@@ -3,7 +3,7 @@
 const emoji = require('node-emoji');
 
 const { log } = console;
-log("This is the Logf", process.argv);
+// log("This is the Logf", process.argv);
 if (process.argv.length <= 2) {
   log(`Usage: ${__filename} emoji_text`);
   process.exit(-1);
@@ -11,7 +11,10 @@ if (process.argv.length <= 2) {
 
 // const result = emoji.emojify(process.argv[2]);
 
+
+//NOTE: The first two commas enables us to ignore and throw away the first two elements of the process.argv to array. We could have also done this:
 const [, , ...argArray] = process.argv;
+// NOTE: process.argv = to the argument that are pass to the node process when the job is started on the terminal i.e. node file.js arg...
 const message = argArray.join(' ');
 const result = emoji.emojify(message);
 
